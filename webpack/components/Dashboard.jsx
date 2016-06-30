1fed476380e6c830e16b4fb4ccf402302a810107
Lindsey <lindsey.font@gmail.com>
2016-06-30 16:04:32 -0600

    Dashboard component

---
+++ webpack/components/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Dashboard extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return <div>Dashboard</div>;
+    }
+}
+
+export default Dashboard;
