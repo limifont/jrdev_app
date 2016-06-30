12153e180cdb2d91780adbdff1ba25d958a6acb1
Lindsey <lindsey.font@gmail.com>
2016-06-30 16:21:14 -0600

    Instructions component

---
+++ webpack/components/Instructions.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Instructions extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return <div>Instructions</div>;
+    }
+}
+
+export default Instructions;
