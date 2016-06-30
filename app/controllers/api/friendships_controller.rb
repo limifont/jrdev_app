092e1524921a29fab7baef30b84c3aefdc8cf779
Henry Doan <henrydoan96@gmail.com>
2016-06-30 17:26:30 -0600

    controller for classroom and friendship

---
+++ app/controllers/api/friendships_controller.rb
@@ -0,0 +1,20 @@
+class Api::FriendshipsController < ApplicationController
+  def index
+		@users = User.all
+	end
+
+	def create
+		@friendship = Friendship.create(user_id: current_user.id, friend_id: params[:friend_id])
+		@inverse = Friendship.create(user_id: params[:friend_id], friend_id: current_user.id)
+		redirect_to root_path			
+	end
+
+	def destroy
+	end
+
+	private
+
+	def set_user
+		@user = User.find(params)
+	end
+end
