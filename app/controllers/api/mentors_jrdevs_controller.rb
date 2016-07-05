637e25b18b76ad69dd869a177f79d7a43e2ba60b
mghadley <matthew.g.hadley@gmail.com>
2016-07-04 20:23:05 -0600

    created controller method for adding mentee via secret pass phrase

---
+++ app/controllers/api/mentors_jrdevs_controller.rb
@@ -0,0 +1,13 @@
+class Api::MentorsJrdevsController < ApplicationController
+	def create
+		if current_user.type == "Mentor"
+			jrdev = Jrdev.find_by(secret_phrase: params[:secret_phrase])
+			mentors_jrdev = MentorsJrdev.new(mentor_id: current_user.id, jrdev_id: jrdev.id)
+			if mentors_jrdev.save
+				render json: jrdev
+			else
+				render json: { errors: mentors_jrdev.errors.full_messages }
+			end
+		end
+	end
+end
