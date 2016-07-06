b7ecd135cc6ff6462e392ee54f7693946d1374ec
Henry Doan <henrydoan96@gmail.com>
2016-07-06 16:46:03 -0600

    achievement working

--- webpack/components/Achievement.jsx
+++ webpack/components/Achievement.jsx
@@ -2,43 +2,50 @@ import React from 'react';
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 
 class Achievement extends React.Component {
  	constructor(props) {
       super(props);
-      this.state = { open: false };
+      this.state = { open: true };
       this.handleClose = this.handleClose.bind(this);
-      this.handleOpen = this.handleOpen.bind(this);
+      // this.handleOpen = this.handleOpen.bind(this);
 
     }
 
-  handleOpen = () => {
-    this.setState({open: true});
-  };
+  // handleOpen = () => {
+  //   this.setState({open: true});
+  // };
 
   handleClose = () => {
-    this.setState({open: false});
+    this.setState({ open: false });
   };
 
   render() {
-    const standardActions = (
+    const standardActions = [
       <FlatButton
         label="X"
         primary={true}
-        onTouchTap={this.handleClose}
-      />
-    );
+        onClick={this.handleClose}
+      />,
+    ];
 
   	return (
-      <div>
-        <Dialog
-          open={this.state.open}
-          title="Congratulations!"
-          onRequestClose={this.handleClose}
-        >
-          Good Job
-        </Dialog>
-      </div>
+      <MuiThemeProvider>
+        <div>
+          <Dialog
+            ref="dialog"
+            open={this.state.open}
+            title="Congratulations!"
+            actions={standardActions}
+            onRequestClose={this.handleClose}
+          >
+            Good Job
+          </Dialog>
+          <p>Test</p>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
