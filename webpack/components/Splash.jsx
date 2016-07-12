63e4882ad831c799b9197499add32f42de3151e5
Henry Doan <henrydoan96@gmail.com>
2016-07-11 19:10:37 -0600

    fixed div size on splash

--- webpack/components/Splash.jsx
+++ webpack/components/Splash.jsx
@@ -61,7 +61,7 @@ const Splash = () => (
         <div className="section">
 
           <div className="row">
-            <div className="col s12 center">
+            <div className="col s12 center" style={{height: 400}}>
               <h4>Contact Us</h4>
               <p className="left-align light center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque id nunc nec volutpat. Etiam pellentesque tristique arcu, non consequat magna fermentum ac. Cras ut ultricies eros. Maecenas eros justo, ullamcorper a sapien id, viverra ultrices eros. Morbi sem neque, posuere et pretium eget, bibendum sollicitudin lacus. Aliquam eleifend sollicitudin diam, eu mattis nisl maximus sed. Nulla imperdiet semper molestie. Morbi massa odio, condimentum sed ipsum ac, gravida ultrices erat. Nullam eget dignissim mauris, non tristique erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
             </div>
