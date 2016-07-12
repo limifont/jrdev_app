5f2ee15d792af0f0189ba878f318c5200c8bc94f
Henry Doan <henrydoan96@gmail.com>
2016-07-11 19:34:30 -0600

    fixed color for footer

--- webpack/components/Footer.jsx
+++ webpack/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router';
 
 const Footer = () => (
 		<footer className="page-footer">
-		  <div className="footer-copyright">
+		  <div className="footer-copyright" style={{ backgroundColor: '#00838F'}}>
 		    <div className="container " >
 			    © 2016 Jr. Devs
 					<Link to='/Splash' className='right white-text'>About Us</Link>
