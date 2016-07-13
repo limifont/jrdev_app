824154db0dc46a296bdc8487a6d7437170e468f5
mghadley <matthew.g.hadley@gmail.com>
2016-07-12 18:28:31 -0600

    finished basic tests for exercise model

--- spec/models/exercise_spec.rb
+++ spec/models/exercise_spec.rb
@@ -19,28 +19,44 @@ RSpec.describe Exercise, type: :model do
 	describe 'attributes' do
 		it { should belong_to :lesson }
 	end
+
+	context 'with no exercises completed' do
+		describe 'last?' do
+			it 'should return true as exercise is last' do
+				expect(exercise.last?(@lesson)).to eq(true)
+			end
+		end
+
+		describe 'first?' do
+			it 'should return true as exercise is first' do
+				expect(exercise.first?(@lesson)).to eq(true)
+			end
+		end
+
+		describe 'completed?' do
+			it 'should return false as the exercise is not complted' do
+				expect(exercise.completed?(user)).to eq(false)
+			end
+		end
+
+		describe 'next_up?' do
+			it 'should return false as not exercises are completed' do
+				expect(exercise.next_up?(user)).to eq(false)
+			end
+		end
+	end
 	
-	describe 'last?' do
-		it 'should return true as exercise is last' do
-			expect(exercise.last?(@lesson)).to eq(true)
+	context 'with an exercise completed' do
+		before(:each) do
+			CompletedExercise.find_or_create_by(user_id: user.id, exercise_id: exercise.id)
 		end
-	end
 
-	describe 'first?' do
-		it 'should return true as exercise is first' do
-			expect(exercise.first?(@lesson)).to eq(true)
+		it 'should return true for completed exercise' do
+			expect(exercise.completed?(user)).to eq(true)
 		end
-	end
 
-	describe 'completed?' do
-		it 'should return false as the exercise is not complted' do
-			expect(exercise.completed?(user)).to eq(false)
-		end
-	end
-
-	describe 'next_up?' do
-		it 'should return false as not exercises are completed' do
-			expect(exercise.next_up?(user)).to eq(false)
+		it 'should return true for next_up' do
+			expect(exercise2.next_up?(user)).to eq(true)
 		end
 	end
 
