75a87883269e73a87a46e6cc30c092b21be74561
mghadley <matthew.g.hadley@gmail.com>
2016-07-12 20:15:38 -0600

    removed find_or_create_by for users in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 15.times do |num|
-	User.find_or_create_by(username: "jrdev#{num}", password: "password", email: "jrdev#{num}@gmail.com", name: "jrdev#{num}", type: "Jrdev")
+	User.create(username: "jrdev#{num}", password: "password", password_confirmation: "password", email: "jrdev#{num}@gmail.com", name: "jrdev#{num}", type: "Jrdev")
 end
 
-educator =	User.find_or_create_by(username: "teacher", password: "password", email: "teacher@gmail.com", name: "teacher", type: "Educator")
+educator =	User.create(username: "teacher", password: "password", password_confirmation: "password", email: "teacher@gmail.com", name: "teacher", type: "Educator")
 
-mentor = User.find_or_create_by(username: "mentor", password: "password", email: "mentor@gmail.com", name: "mentor", type: "Mentor")
+mentor = User.create(username: "mentor", password: "password", password_confirmation: "password", email: "mentor@gmail.com", name: "mentor", type: "Mentor")
 
-lesson1 = Lesson.find_or_create_by(name: "Intro to Programming")
+lesson1 = Lesson.create(name: "Intro to Programming")
 exercise1_1 = Exercise.find_or_create_by(
 	lesson_id: lesson1.id, 
 	name: "What is Code", 
