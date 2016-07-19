86dc35704fe5be72acf72ed1978fbb014fc78550
Lindsey <lindsey.font@gmail.com>
2016-07-19 10:22:12 -0600

    Small changes

--- webpack/components/Lessons.jsx
+++ webpack/components/Lessons.jsx
@@ -7,13 +7,41 @@ class Lessons extends React.Component {
 	constructor(props) {
 		super(props)
 		this.displayLessons = this.displayLessons.bind(this)
+		this.displayCheck = this.displayCheck.bind(this)
 	}
 
 	displayLessonName(lesson) {
 		if(this.props.links) {
 			return(<h4 className="lessons-title"><Link to={`/lesson/${lesson.id}`}>{lesson.name}</Link></h4>)
 		} else {
-			return(<h4>{lesson.name}</h4>)
+			return(<h4 className="lessons-title">{lesson.name}</h4>)
+		}
+	}
+
+	displayCheck(check) {
+		if(check === 'true') {
+			return (
+				<i className="material-icons">done</i>
+			)
+		}
+	}
+
+	progressBar(lesson) {
+		let complete = lesson.exercises_completed_count
+		let total = lesson.exercises_count
+		let num = Math.round( (complete/total) * 100 )
+		let check = lesson.completed.toString()
+		if(check === 'false') {
+			return(
+				<div className="row valign-wrapper">  
+				  <div className="valign col s9 offset-s1">
+				  	<LinearProgress mode="determinate" max={lesson.exercises_count} value={lesson.exercises_completed_count}  />
+				  </div>
+				  <div className="valign col s2">
+				  	<p>{num}%</p>
+				  </div>
+				</div>
+			)
 		}
 	}
 
@@ -22,56 +50,25 @@ class Lessons extends React.Component {
 			let check = lesson.completed.toString()
 			let icon = lesson.icon_url
 			console.log(icon);
-			if(check === 'true' ) {
-				return(
-					<MuiThemeProvider key={`lesson-${lesson.id}`}>
-						<div className="col s12 m12">
-							<div className="card">	
-								<div className="card-content">
-									<div className="row" style={{overflow: 'hide'}}>
-										<div className="col s3 m3 l3" style={{maxWidth: '100%', maxHeight: '100%'}}>
-											<img src={icon} style={{maxWidth: '100%', maxHeight: '100%'}}/>
-										</div>
-										<div className="col s9 m9 l9">
-			            		<h3 className="lessons-title">{this.displayLessonName(lesson)}<i className="material-icons">done</i></h3>
-			            	</div>
+			return(
+				<MuiThemeProvider key={`lesson-${lesson.id}`}>
+					<div className="col s12 m12">
+						<div className="card">	
+							<div className="card-content">
+								<div className="row valign-wrapper">
+									<div className="valign col s3" style={{maxWidth: '100%', maxHeight: '100%'}}>
+										<img src={icon} style={{maxWidth: '100%', maxHeight: '100%'}}/>
 									</div>
-			          </div>
-			        </div>
+									<div className="valign col s9 left-align">
+		            		{this.displayLessonName(lesson)}{this.displayCheck(check)}
+		            	</div>
+								</div>
+								{this.progressBar(lesson)}
+		          </div>
 		        </div>
-			    </MuiThemeProvider>
-				)
-			} else {
-				let complete = lesson.exercises_completed_count
-				let total = lesson.exercises_count
-				let num = Math.round( (complete/total) * 100 )
-				return(
-					<MuiThemeProvider key={`lesson-${lesson.id}`}>
-						<div className="col s12 m12">
-							<div className="card">		
-								<div className="card-content">
-			            <div className="row valign-wrapper">
-			            	<div className="valign col s3" style={{maxWidth: '100%', maxHeight: '100%'}}>
-											<img src={icon} style={{maxWidth: '100%', maxHeight: '100%'}}/>
-										</div>
-			            	<div className="valign col s9 left-align">
-			            		{this.displayLessonName(lesson)}
-			            	</div>
-			            </div>
-				          <div className="row valign-wrapper">  
-				            <div className="valign col s11">
-				            	<LinearProgress mode="determinate" max={lesson.exercises_count} value={lesson.exercises_completed_count}  />
-				            </div>
-				            <div className="valign col s1">
-				            	<p>{num}%</p>
-				            </div>
-				          </div>
-			          </div>
-			    		</div>
-			    	</div>      	
-			    </MuiThemeProvider>
-				)
-			}
+	        </div>
+		    </MuiThemeProvider>
+			)
 		})
 	}
 
@@ -80,7 +77,7 @@ class Lessons extends React.Component {
 			<div>
 				{this.displayLessons()}
 			</div>
-		);
+		)
 	}
 }
 
